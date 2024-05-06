72b9999605cd9fc57ca2268809647412c39f20c6
unknown <blgzgg@umsystem.edu>
2024-05-06 01:34:26 -0500

    added ticket generation and updated style

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for, flash
 import sqlite3
+import string
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
+db_path = os.path.join(base_dir, 'dbs', 'reservations.db')
 
 def get_cost_matrix():
     cost_matrix = [[100, 75, 50, 100] for row in range(12)]
     return cost_matrix
 
 def calculate_total_sales():
-    conn = sqlite3.connect('dbs/reservations.db')
+    conn = sqlite3.connect(db_path)
     c = conn.cursor()
     c.execute("SELECT seatRow, seatColumn FROM reservations")
     reserved_seats = c.fetchall()
@@ -23,12 +28,26 @@ def calculate_total_sales():
     return total_sales
 
 def get_reserved_seats():
-    conn = sqlite3.connect('dbs/reservations.db')
+    conn = sqlite3.connect(db_path)
     c = conn.cursor()
     c.execute("SELECT seatRow, seatColumn FROM reservations")
     reserved_seats = c.fetchall()
     conn.close()
     return reserved_seats
+
+# Generate new eTicketNumbers
+def generate_eTicketNumber(first_name):
+    target_string = 'INFOTC4320'
+    result = ''
+    target_index = 0
+
+    for letter in first_name:
+        result += target_string[target_index] + letter
+        target_index += 1
+
+    result += target_string[target_index:]
+
+    return result
 
 @app.route('/')
 def main_menu():
@@ -39,7 +58,7 @@ def admin_login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        conn = sqlite3.connect('dbs/reservations.db')
+        conn = sqlite3.connect(db_path)
         c = conn.cursor()
         c.execute("SELECT * FROM admins WHERE username=? AND password=?", (username, password))
         admin = c.fetchone()
@@ -68,20 +87,21 @@ def reserve_seat():
             return render_template('reserve_seat.html', cost_matrix=get_cost_matrix(), reserved_seats=reserved_seats, error=error)
         
         # If seat not reserved, proceed with reservation
-        conn = sqlite3.connect('dbs/reservations.db')
+        eTicketNumber = generate_eTicketNumber(first_name)
+        conn = sqlite3.connect(db_path)
         c = conn.cursor()
-        c.execute("INSERT INTO reservations (passengerName, seatRow, seatColumn, eTicketNumber) VALUES (?, ?, ?, ?)", (first_name + " " + last_name, seat_row, seat_column, 'ETN123'))  # Generating eTicketNumber temporarily
+        c.execute("INSERT INTO reservations (passengerName, seatRow, seatColumn, eTicketNumber) VALUES (?, ?, ?, ?)", (first_name + " " + last_name, seat_row, seat_column, eTicketNumber))
         conn.commit()
         conn.close()
         
         # Redirect to reservation confirmation page
-        return redirect(url_for('reserve_confirmation', first_name=first_name, last_name=last_name))
+        return redirect(url_for('reserve_confirmation', first_name=first_name, last_name=last_name, eTicketNumber=eTicketNumber))
     
     return render_template('reserve_seat.html', cost_matrix=get_cost_matrix(), reserved_seats=get_reserved_seats())
 
-@app.route('/reserve-confirmation/<first_name>/<last_name>')
-def reserve_confirmation(first_name, last_name):
-    return render_template('reserve_confirmation.html', first_name=first_name, last_name=last_name)
+@app.route('/reserve-confirmation/<first_name>/<last_name>/<eTicketNumber>')
+def reserve_confirmation(first_name, last_name, eTicketNumber):
+    return render_template('reserve_confirmation.html', first_name=first_name, last_name=last_name, eTicketNumber=eTicketNumber)
 
 
 if __name__ == '__main__':
